0d644b4f2909992442714908454a14ba943df3c9
sarabenko <sarabenkwider@gmail.com>
2019-08-29 16:40:35 +0200

    Add files via upload
    commit projet de test technique

---
+++ TaskExecutionServiceImpl.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package com.univers.architecture.transporter.service;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import com.univers.architecture.transporter.dao.ITaskExecutionRepository;
+import com.univers.architecture.transporter.model.TaskExecution;
+
+
+
+/**
+ * @author sabir
+ *
+ */
+@Service
+public class TaskExecutionServiceImpl implements ITaskExecutionService {
+
+	private static final Logger log = LoggerFactory.getLogger(TaskExecutionServiceImpl.class);
+
+	@Autowired
+	private ITaskExecutionRepository taskExecutionRepository;
+
+	@Override
+	public List<TaskExecution> getAllTaskExecution(int page, int size) {
+
+		log.warn("Loading all taskExecution ! be careful about performance");
+		
+		return (List<TaskExecution>) this.taskExecutionRepository.findPageTaskExecution(new PageRequest(page, size));
+	}
+
+}
